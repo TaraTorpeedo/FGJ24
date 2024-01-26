919f6f011ab0954764d18f253f392b0407f0bebc
RikuH <54583576+TaraTorpeedo@users.noreply.github.com>
2024-01-26 20:36:18 +0200

    Audio detection

--- Assets/AudioDetection.cs
+++ Assets/AudioDetection.cs
@@ -1,28 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioDetection : MonoBehaviour
 {
-    public int sampleWindow = 64;
+    [SerializeField] private int sampleWindow = 64;
+    private AudioClip microphoneClip;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Start() => MicrophoneToAudioClip();
+
+    public void MicrophoneToAudioClip()
     {
-        
+        string microphoneName = Microphone.devices[0];
+        microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float GetLoudnessFromMicrophone() => GetLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
 
-    public float GetLoudnessFromAudioClip(int clipPos, AudioClip audioClip)
-    {  
+    private float GetLoudnessFromAudioClip(int clipPos, AudioClip audioClip)
+    {
         int startPos = clipPos - sampleWindow;
 
-        if(startPos < 0)
+        if (startPos < 0)
         {
             return 0;
         }
@@ -39,5 +36,29 @@ public class AudioDetection : MonoBehaviour
         }
 
         return totalLoudness / sampleWindow;
-    }  
+    }
+
+    //Goblin king will have this
+
+    /*
+     [SerializeField] private AudioDetection audioDetector;
+    [SerializeField] private float loudnessSensibility = 100;
+    [SerializeField] private float threshold = 0.1f;
+    [SerializeField] private GameObject player;
+
+    public void Update()
+    {
+        float loudness = audioDetector.GetLoudnessFromMicrophone() * loudnessSensibility;
+
+        if (loudness >= threshold)
+        {
+        transform.localScale = Vector3.Lerp(new Vector3(1, 1, 1), new Vector3(2, 2, 2), loudness);
+        }
+        else
+        {
+            loudness = 0;
+        }
+    }
+
+     */
 }
